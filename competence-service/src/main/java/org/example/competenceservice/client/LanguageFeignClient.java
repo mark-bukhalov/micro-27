7039e58256be8e0ea45d7f585f39e4b9172f0553
Mark <mark.bukhalov98@gmail.com>
2024-11-24 14:23:13 +0300

    fix CircuitBreaker

--- competence-service/src/main/java/org/example/competenceservice/client/LanguageFeignClient.java
+++ competence-service/src/main/java/org/example/competenceservice/client/LanguageFeignClient.java
@@ -1,5 +1,6 @@
 package org.example.competenceservice.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.example.competenceservice.usecases.dto.LanguageResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,11 @@ import java.util.List;
 @FeignClient(name = "language-service")
 public interface LanguageFeignClient {
 
+    @CircuitBreaker(name = "competence-service", fallbackMethod = "fallback")
     @GetMapping("/api/v1/languages")
     List<LanguageResponseDto> getLanguageListByIdList(@RequestParam(name = "ids") List<Long> idList);
+
+    default List<LanguageResponseDto> fallback(Throwable throwable) {
+        return List.of(new LanguageResponseDto(1L, "NOTFOUND"));
+    }
 }
